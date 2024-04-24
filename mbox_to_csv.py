3f40e4dc71e6c288b42879336e1b6c98a0c60584
jackrich78 <jackrich78@gmail.com>
2024-04-24 11:40:30 +0200

    Initial commit with data organised

--- mbox_to_csv.py
+++ mbox_to_csv.py
@@ -38,6 +38,6 @@ def mbox_to_csv(mbox_file_path, csv_file_path):
             ])
 
 # Usage
-mbox_file_path = 'Sent.mbox'  # replace with the path to your MBOX file
-csv_file_path = 'past_email_mbox.csv'  # replace with the desired path for the output CSV file
+mbox_file_path = 'inbox-manager-agent/data/raw_emails.mbox'  # replace with the path to your MBOX file
+csv_file_path = 'inbox-manager-agent/data/past_email_mbox.csv'  # replace with the desired path for the output CSV file
 mbox_to_csv(mbox_file_path, csv_file_path)
